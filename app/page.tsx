bfe36fcc2ac915fc7903e7089ed40912b9322d4a
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2024-10-10 03:46:43 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <HomeHeroSection />
       <HomeBanarSection />
-      <HomeSubscriptionSection />
+{/*       <HomeSubscriptionSection /> */}
       <HomeCustomerReview />
     </>
   );
