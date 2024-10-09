05424892662953df5ca38843a517d74f8e3744d4
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2024-10-10 03:48:48 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import HomeBanarSection from "@/components/Home/HomeBanarSection";
 import HomeCustomerReview from "@/components/Home/HomeCustomerReview";
 import HomeHeroSection from "@/components/Home/HomeHeroSection";
-import HomeSubscriptionSection from "@/components/Home/HomeSubscriptionSection";
+// import HomeSubscriptionSection from "@/components/Home/HomeSubscriptionSection";
 
 export default function Home() {
   return (
