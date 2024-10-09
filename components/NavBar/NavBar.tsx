a0a07f562c1ca34b80eee636254bfca8386775c0
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2024-10-10 04:01:45 +0600

    Update NavBar.tsx

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -21,8 +21,8 @@ import { navigationMenuTriggerStyle } from "../ui/navigation-menu";
 
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<"a">,HTMLAnchorElement, InternalLinkProps
+>(({ className, title,href="/", children, ...props }, ref) => {
   const { isBangla } = useLang();
   return (
     <li>
@@ -33,6 +33,7 @@ const ListItem = React.forwardRef<
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
           )}
+          href={href}
           {...props}
         >
           <div
