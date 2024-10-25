c6fdaff1e08ade0ea66a8ef6170730f37e502674
MD Forhadul Islam <mdforhadul44@gmail.com>
2024-10-25 22:23:20 +0600

    adddd alll complite all

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import FooterBar from "@/components/FooterBar/FooterBar";
 import Navbar from "@/components/NavBar/NavBar";
-import type { Metadata } from "next";
-import "./globals.css";
 import LangContextProvider from "@/context/LangContext";
 import LoadContextProvider from "@/context/LoadContext";
-import { ContextMenu, ContextMenuTrigger } from "@radix-ui/react-context-menu";
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Finex - Home",
