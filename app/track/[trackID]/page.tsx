b47e2f50b1fd34dea282788d070e9a90fa042716
MD Forhadul Islam <mdforhadul44@gmail.com>
2024-10-27 23:02:14 +0600

    add all

--- app/track/[trackID]/page.tsx
+++ app/track/[trackID]/page.tsx
@@ -2,17 +2,17 @@
 
 import ShipmentTrackingBox from "@/components/Track/ShipmentTrackingBox";
 import { ShipmentTrackingDetails } from "@/components/Track/ShipmentTrackingDetails";
-import { useParams,  } from "next/navigation";
-
+import { useParams } from "next/navigation";
 
 const ShipmentTrack = () => {
   const searchParams = useParams();
-  const trackID = searchParams.trackID; 
+  const trackID = Array.isArray(searchParams.trackID) ? searchParams.trackID[0] : searchParams.trackID;
 
-  return <>
-  <ShipmentTrackingBox />
-  <ShipmentTrackingDetails trackID={trackID} />
-  </>;
+  return (
+    <>
+      <ShipmentTrackingBox />
+      <ShipmentTrackingDetails trackID={trackID} />
+    </>
+  );
 };
-
 export default ShipmentTrack;
